19daba1f4c8bcba78b09b55893e7daf28b09dcf3
Jayaraman <adarsh1999@gmail.com>
2020-12-05 12:06:41 +0530

    Updated about page

--- Website/src/pages/About.vue
+++ Website/src/pages/About.vue
@@ -1,7 +1,108 @@
 <template>
 <div class="flex m-auto w-10/11 justify-center h-screen">
-    <h1 class="font-bold text-5xl">About us</h1>
+    <title>About</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+<!-- <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css"> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div id="myPage">
+
+
+<br><br>
+<div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+  <div class="w3-display-middle" style="white-space:nowrap;">
+    <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">FINAL<span class="w3-hide-small"> YEAR </span> PROJECT </span>
+  </div>
 </div>
+<br><br>
+<!-- Container (About Section) -->
+<div class="">
+  <h2 class="w3-center text-3xl font-bold">ABOUT THE PROJECT</h2>
+  <p class="w3-center text-xs"><em>Multiple Choice Q&A and Distractor Generation using Text-Transformers</em></p>
+  <p class="text-justify m-12 text-xl w-8/12 mx-auto">With so much digital content to learn from, it is almost impossible to test one's understanding. It is also 
+      pretty difficult to ask someone to prepare questions based on the learned content, So we built this automatic MCQ generator which works with any form
+      of text and provides candidate MCQ questions with suitable distractors with which one can test their understanding of the content. The question generator is
+      a fine-tuned T5-base model for question generation task.</p>
+  <center>
+    <h6><a href="https://www.overleaf.com/project/5f8628c048052f0001387df7" class="text-blue-500 underline">More About This Project</a></h6>
+  </center>
+</div>
+
+<div class="w3-container w3-padding-64 w3-center">
+<h2 class="text-3xl font-bold">OUR TEAM</h2>
+<p class="w3-center text-xs"><em>SSN College of Engineering - Dept. of Computer Science & Engineering</em></p>
+<br>
+<div class="w3-row"><br>
+
+
+<center>
+<div class="w3-third">
+  <img src="https://avatars2.githubusercontent.com/u/36816700?s=400&u=37a0f987bb17383543edc36ab9a0cf7bcb00cfab&v=4" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Jayaraman N R</h3>
+  <p>Final Year</p>
+</div>
+
+<div class="w3-third">
+  <img src="https://scontent-maa2-1.xx.fbcdn.net/v/t1.0-9/44038416_2058879604423241_8858734458795196416_o.jpg?_nc_cat=109&cb=846ca55b-311e05c7&ccb=2&_nc_sid=09cbfe&_nc_ohc=lkFQ-kIsYsMAX8J3-Hb&_nc_oc=AQlV94DPG7jRm6Guik3CB056FGVgybm-Dt05KjQzNRswRBUCk8TSjiWu9_JrsQqjE2M&_nc_ht=scontent-maa2-1.xx&oh=08d6b7d4c916afc97bec7c317ac80f84&oe=5FEECD7D" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Gokul Sahar</h3>
+  <p>Final Year</p>
+</div>
+
+<div class="w3-third">
+  <img src="https://scontent-maa2-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/42611108_2260547674165051_6098128567871628914_n.jpg?_nc_ht=scontent-maa2-2.cdninstagram.com&_nc_cat=107&_nc_ohc=vtF9tNCumJEAX8y8rmJ&tp=1&oh=a0e8d300deff5e8500b850cbd0e41ad3&oe=5FF2E591" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Dinesh K</h3>
+  <p>Final Year</p>
+</div>
+</center>
+</div>
+</div>
+
+
+<!-- Image of location/map -->
+<!-- <img src="/w3images/map.jpg" class="w3-image w3-greyscale-min" style="width:100%;"> -->
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
+  <h4>Contact Us</h4>
+  <div class="flex w-full justify-around mt-10">
+      <div class="justify-between">
+  <!-- <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Facebook"><i class="fa fa-facebook"></i></a> -->
+  <!-- <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Twitter"><i class="fa fa-twitter"></i></a> -->
+  <!-- <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-google-plus"></i></a> -->
+    <a class="w3-button w3-large" href="https://github.com/range123" title="Github"><i class="fa fa-github"></i></a>
+  <a class="w3-button w3-large w3-hide-small" href="https://www.linkedin.com/in/jayaraman-n-r-884818193/" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+  <!-- <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-instagram"></i></a> -->
+  <!-- <a class="w3-button w3-large w3-teal w3-hide-small" href="javascript:void(0)" title="Linkedin"><i class="fa fa-google-plus"></i></a> -->
+ </div>
+       <div class="justify-between">
+  <!-- <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Facebook"><i class="fa fa-facebook"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Twitter"><i class="fa fa-twitter"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-google-plus"></i></a> -->
+  <a class="w3-button w3-large" href="https://github.com/gokulsahar" title="Github"><i class="fa fa-github"></i></a>
+  <a class="w3-button w3-large w3-hide-small" href="https://www.linkedin.com/in/gokulsahar/" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+ </div>
+       <div class="justify-between">
+  <!-- <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Facebook"><i class="fa fa-facebook"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Twitter"><i class="fa fa-twitter"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-google-plus"></i></a>
+  <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-instagram"></i></a> -->
+    <a class="w3-button w3-large" href="" title="Github"><i class="fa fa-github"></i></a>
+  <!-- <a class="w3-button w3-large w3-teal w3-hide-small" href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin"></i></a> -->
+  <a class="w3-button w3-large w3-hide-small" href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+ </div>
+  </div>
+  
+
+  <div style="position:relative;bottom:100px;z-index:1;" class="w3-tooltip w3-right">
+    <span class="w3-text w3-padding w3-hide-small">Go To Top</span>   
+    <a class="w3-button w3-theme" href="#myPage"><span class="w3-xlarge">
+    <i class="fa fa-chevron-circle-up"></i></span></a>
+  </div>
+</footer>
+</div>
+</div>
+
 
 </template>
 
@@ -11,6 +112,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped src="../assets/w3.css">
 
 </style>
